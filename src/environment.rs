d97b29f2eb79ec0801c871b632c721ecc65274e4
LeandroVandari <bernardokreutz@gmail.com>
2025-07-31 15:48:32 -0300

    Updated ground so it's a single object

--- src/environment.rs
+++ src/environment.rs
@@ -1,9 +1,10 @@
 use avian2d::prelude::*;
-use bevy::prelude::*;
+use bevy::{prelude::*, window::PrimaryWindow};
 
 pub struct EnvironmentPlugin;
 
 const TILE_SIZE: f32 = 20.;
+const LEVEL_WIDTH: usize = 200;
 
 impl Plugin for EnvironmentPlugin {
     fn build(&self, app: &mut App) {
@@ -16,17 +17,19 @@ impl EnvironmentPlugin {
         mut commands: Commands,
         mut meshes: ResMut<Assets<Mesh>>,
         mut materials: ResMut<Assets<ColorMaterial>>,
+        window: Query<&Window, With<PrimaryWindow>>
     ) {
-        for i in 0..20 {
+        let window_size = window.iter().next().unwrap().size();
+
             commands.spawn((
                 Mesh2d(meshes.add(Rectangle {
-                    half_size: vec2(TILE_SIZE / 2., TILE_SIZE / 2.),
+                    half_size: vec2(TILE_SIZE*LEVEL_WIDTH as f32/2., TILE_SIZE / 2.),
                 })),
                 MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::WHITE))),
                 RigidBody::Static,
-                Collider::rectangle(TILE_SIZE, TILE_SIZE),
-                Transform::from_xyz(-100. + i as f32 * TILE_SIZE, -200., 0.0),
+                Collider::rectangle(TILE_SIZE*LEVEL_WIDTH as f32, TILE_SIZE),
+                Transform::from_xyz(-window_size.x/2., -window_size.y /2. + TILE_SIZE+10., 0.0),
             ));
-        }
+        
     }
 }
