202763c10b7940cc96f84fd5f3501ec4770a9425
LeandroVandari <bernardokreutz@gmail.com>
2025-07-31 01:05:45 -0300

    updated features

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
+use avian2d::prelude::*;
 use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Stop yourself".into(),
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Stop yourself".into(),
+                    ..Default::default()
+                }),
                 ..Default::default()
             }),
-            ..Default::default()
-        }), PhysicsPlugins::default()))
+            PhysicsPlugins::default(),
+        ))
         .add_plugins(gmtk::player::PlayerPlugin)
+        .insert_resource(Gravity(Vec2::NEG_Y * 100.))
         .insert_resource(ClearColor(Color::BLACK))
         .run();
-
 }
--- src/player.rs
+++ src/player.rs
@@ -1,18 +1,20 @@
-use bevy::prelude::*;
 use avian2d::prelude::*;
+use bevy::prelude::*;
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Setup, Self::spawn_player)
+        app.add_systems(Setup, Self::spawn_player);
     }
 }
 
 impl PlayerPlugin {
     fn spawn_player(mut commands: Commands) {
         commands.spawn((
-            RigidBody::Dynamic
+            RigidBody::Dynamic,
+            Collider::rectangle(20., 100.),
+            Transform::from_xyz(0.0, 0.0, 0.0),
         ));
     }
 }
