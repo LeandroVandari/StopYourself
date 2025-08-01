10a3ab7b9bc921b4eff91d10aa876961bbd78281
LeandroVandari <bernardokreutz@gmail.com>
2025-08-01 00:23:38 -0300

    added camera movement on the defend mode

--- src/camera.rs
+++ src/camera.rs
@@ -1,6 +1,6 @@
 use bevy::{prelude::*, window::PrimaryWindow};
 
-use crate::{LevelDimensions, player::Player};
+use crate::{LevelDimensions, modes::GameMode, player::Player};
 
 pub struct CameraPlugin;
 /// How many tiles ahead of the player the camera should be.
@@ -8,8 +8,14 @@ const CAMERA_AHEAD: usize = 15;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut bevy::app::App) {
-        app.add_systems(Startup, Self::spawn_camera)
-            .add_systems(Update, Self::follow_player);
+        app.add_systems(Startup, Self::spawn_camera).add_systems(
+            Update,
+            (
+                Self::follow_player
+                    .run_if(in_state(GameMode::Survive).or(in_state(GameMode::Replay))),
+                Self::keyboard_input.run_if(in_state(GameMode::Defend)),
+            ),
+        );
     }
 }
 
@@ -27,7 +33,7 @@ impl CameraPlugin {
     ) {
         let target_translation = vec3(
             (player.translation.x + level_dimensions.tile_size * CAMERA_AHEAD as f32)
-                .max(level_dimensions.start.x + main_window.size().x / 2.)
+                .max(level_dimensions.start.x + main_window.size().x / 2.) // Clamp to the level size
                 .min(
                     level_dimensions.start.x
                         + level_dimensions.tile_size * level_dimensions.level_length as f32
@@ -46,5 +52,37 @@ impl CameraPlugin {
         let dir = diff.clone().normalize();
 
         camera.translation += dir * f32::min(10., diff_length);
+    }
+
+    /// Move the camera with keyboard on defend mode
+    fn keyboard_input(
+        keyboard: Res<ButtonInput<KeyCode>>,
+        mut camera: Single<&mut Transform, With<Camera2d>>,
+        level_dimensions: Res<LevelDimensions>,
+        main_window: Single<&Window, With<PrimaryWindow>>,
+    ) {
+        if keyboard.any_pressed([KeyCode::KeyA, KeyCode::ArrowLeft]) {
+            camera.translation.x -= 10.;
+        }
+        if keyboard.any_pressed([KeyCode::KeyD, KeyCode::ArrowRight]) {
+            camera.translation.x += 10.;
+        }
+        if keyboard.any_pressed([KeyCode::KeyW, KeyCode::ArrowUp]) {
+            camera.translation.y += 10.;
+        }
+        if keyboard.any_pressed([KeyCode::KeyS, KeyCode::ArrowDown]) {
+            camera.translation.y -= 10.;
+        }
+
+        camera.translation.x = camera
+            .translation
+            .x
+            .max(level_dimensions.start.x + main_window.size().x / 2.) // Clamp to the level size
+            .min(
+                level_dimensions.start.x
+                    + level_dimensions.tile_size * level_dimensions.level_length as f32
+                    - main_window.size().x / 2.,
+            );
+        camera.translation.y = camera.translation.y.max(0.0).min(100.);
     }
 }
