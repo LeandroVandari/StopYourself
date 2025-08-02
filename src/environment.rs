94604e1c0c62a63eb489528387c2e9a5cdecf47b
LeandroVandari <bernardokreutz@gmail.com>
2025-08-02 19:00:56 -0300

    can't walk off the ground :O

--- src/environment.rs
+++ src/environment.rs
@@ -12,7 +12,7 @@ impl Plugin for EnvironmentPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             OnEnter(GameState::Game),
-            (Self::spawn_ground, Self::spawn_goal),
+            (Self::spawn_ground, Self::spawn_goal, Self::spawn_walls),
         )
         .add_event::<ResetEnvironment>();
     }
@@ -25,14 +25,14 @@ impl EnvironmentPlugin {
         commands.spawn((
             Sprite {
                 color: Color::WHITE,
-                custom_size: Some(vec2(ground_width, level_dimensions.tile_size)),
+                custom_size: Some(vec2(ground_width, level_dimensions.tile_size * 3.)),
                 ..Default::default()
             },
             RigidBody::Static,
-            Collider::rectangle(ground_width, level_dimensions.tile_size),
+            Collider::rectangle(ground_width, level_dimensions.tile_size * 3.),
             Transform::from_translation(
                 level_dimensions
-                    .grid_pos_to_pixels((0, 2), vec2(ground_width, level_dimensions.tile_size))
+                    .grid_pos_to_pixels((0, 0), vec2(ground_width, level_dimensions.tile_size * 3.))
                     .extend(0.),
             ),
         ));
@@ -54,7 +54,7 @@ impl EnvironmentPlugin {
                 Collider::rectangle(size.x, size.y),
                 Transform::from_translation(
                     level_dimensions
-                        .grid_pos_to_pixels((level_dimensions.level_length - 10, 3), size)
+                        .grid_pos_to_pixels((level_dimensions.level_length as i32 - 10, 3), size)
                         .extend(0.),
                 ),
             ))
@@ -68,6 +68,20 @@ impl EnvironmentPlugin {
                     }
                 },
             );
+    }
+
+    fn spawn_walls(mut commands: Commands, level_dimensions: Res<LevelDimensions>) {
+        let size = vec2(level_dimensions.tile_size, level_dimensions.tile_size * 40.);
+
+        for x in [-1, level_dimensions.level_length as i32] {
+            commands.spawn((
+                Collider::rectangle(size.x, size.y),
+                RigidBody::Static,
+                Transform::from_translation(
+                    level_dimensions.grid_pos_to_pixels((x, 0), size).extend(0.),
+                ),
+            ));
+        }
     }
 }
 
