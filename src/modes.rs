819375d6a32098eb30ac65e891041513c8e9cd0f
rafer45 <ataque992@hotmail.com>
2025-08-01 20:06:43 -0400

    Add trace of movement

--- src/modes.rs
+++ src/modes.rs
@@ -1,8 +1,12 @@
 use bevy::{diagnostic::FrameCount, prelude::*};
 
 use crate::{
-    environment::ResetEnvironment, obstacles::SpawnGhostObstacleEvent,
-    player::record_movement::RecordedMovements,
+    environment::ResetEnvironment,
+    obstacles::{GhostObstacle, LastInsertedObstacle, SpawnGhostObstacleEvent},
+    player::{
+        Player, PositionEvent, record_movement::RecordedMovements,
+        record_position::RecordedPositions,
+    },
 };
 
 /// The two modes for the game
@@ -29,6 +33,10 @@ impl Plugin for ModesManagement {
             (Self::change_to_defend
                 .run_if(on_event::<GoalReached>.and(in_state(GameMode::Survive))),),
         )
+        .add_systems(
+            FixedUpdate,
+            (Self::draw_player_ghost.run_if(on_event::<GoalReached>),),
+        )
         .add_systems(OnEnter(GameMode::Replay), Self::replay)
         .init_state::<GameMode>()
         .add_event::<GoalReached>();
@@ -36,6 +44,24 @@ impl Plugin for ModesManagement {
 }
 
 impl ModesManagement {
+    fn draw_player_ghost(
+        mut commands: Commands,
+        mut meshes: ResMut<Assets<Mesh>>,
+        mut materials: ResMut<Assets<ColorMaterial>>,
+        recorded_positions: Res<RecordedPositions>,
+    ) {
+        for (_, PositionEvent::Position(recorded_position)) in &recorded_positions.positions {
+            commands.spawn((
+                // Appearance
+                Mesh2d(meshes.add(Rectangle {
+                    half_size: vec2(20., 40.),
+                })),
+                MeshMaterial2d(materials.add(ColorMaterial::from_color(Color::WHITE))),
+                // Movement
+                Transform::from_translation(recorded_position.extend(1.)),
+            ));
+        }
+    }
     fn change_to_defend(
         mut state: ResMut<NextState<GameMode>>,
         mut reset_environment: EventWriter<ResetEnvironment>,
